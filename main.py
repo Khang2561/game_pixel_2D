18801c76b79165bf3a3642f8331363499a5cb247
Khang2561 <21522194@gm.uit.edu.vn>
2023-10-17 00:01:40 +0700

    set up screen, map, debug for game

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame, sys
+from setting import *
+from debug import *
+
+class Game:#create class with name Game
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGTH))#set width and heigth for screen
+        pygame.display.set_caption('Zelda')#set name for window
+        self.clock = pygame.time.Clock()#clock count back to start game
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:#if even is quick, end the game
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill('black')#set up background for screen
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
+    print("complete")
